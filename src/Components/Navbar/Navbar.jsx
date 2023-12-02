93cf5aea019617541e8ca5acca74991e3a18ae74
jackstasiulis <jackstasiulis@gmail.com>
2023-12-02 10:58:53 -0500

    add basic functional navbar

--- src/Components/Navbar/Navbar.jsx
+++ src/Components/Navbar/Navbar.jsx
@@ -1,11 +1,26 @@
 import './Navbar.scss'
+import React, { useState } from 'react';
 
 function Navbar () {
+    const [menuOpen, setMenuOpen] = useState(false);
 
+    const toggleMenu = () => {
+      setMenuOpen(!menuOpen);
+    };
 
     return(
-        <nav className='navbar' >
-            <h1>peens</h1>
+        <nav className='navbar'>
+            <div className='navbar__logo'>Your Logo</div>
+
+            <div className={`navbar__menu ${menuOpen ? 'open' : ''}`}>
+                <div className='navbar__item'>Item 1</div>
+                <div className='navbar__item'>Item 2</div>
+                <div className='navbar__item'>Item 3</div>
+            </div>
+
+            <div className='hamburger-menu' onClick={toggleMenu}>
+                &#9776;
+            </div>
         </nav>
     )
 }
