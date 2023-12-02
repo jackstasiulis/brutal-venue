9072a50369bbccba17d80998acdbae98a3c93f35
jackstasiulis <jackstasiulis@gmail.com>
2023-12-02 11:32:50 -0500

    create basic event section markup structure

---
+++ src/Components/EventCard/EventCard.jsx
@@ -0,0 +1,13 @@
+import './EventCard.scss'
+
+function EventCard () {
+
+
+    return(
+        <body className='event-card'>
+            <h1>EventCard</h1>
+        </body>
+    )
+}
+
+export default EventCard;
---
+++ src/Components/Events/Events.jsx
@@ -0,0 +1,15 @@
+import EventCard from '../EventCard/EventCard';
+import './Events.scss'
+
+function Events () {
+
+
+    return(
+        <body className='events'>
+            <h1>events</h1>
+            <EventCard />
+        </body>
+    )
+}
+
+export default Events;
