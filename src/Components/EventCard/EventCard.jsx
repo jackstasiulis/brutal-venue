d4e1e08e96db97bd52a84d94749c295012fdba63
jackstasiulis <jackstasiulis@gmail.com>
2023-12-02 15:54:01 -0500

    add hover mouse follow effect on event posters

--- src/Components/EventCard/EventCard.jsx
+++ src/Components/EventCard/EventCard.jsx
@@ -1,7 +1,39 @@
 import './EventCard.scss'
+import { useMotionValue, motion, useSpring, useTransform } from 'framer-motion';
+import skellyPic from '../../Assets/skelly.webp'
 
 function EventCard () {
 
+    const x = useMotionValue(0)
+    const y = useMotionValue(0)
+
+    const mouseXSpring = useSpring(x);
+    const mouseYSpring = useSpring(y);
+
+    const rotateX = useTransform(mouseYSpring, [-0.5, 0.5], ['17.5deg','-17.5deg']);
+    const rotateY = useTransform(mouseXSpring, [-0.5, 0.5], ['-17.5deg','17.5deg']);
+
+
+    const handleMouseMove = (e) => {
+        const rect = e.target.getBoundingClientRect();
+
+        const width = rect.width;
+        const height = rect.height;
+
+        const mouseX = e.clientX - rect.left;
+        const mouseY = e.clientY - rect.top;
+
+        const xPct = mouseX / width - 0.5;
+        const yPct = mouseY / height -0.5;
+        
+        x.set(xPct);
+        y.set(yPct);
+    }
+
+    const handleMouseLeave = () => {
+        x.set(0);
+        y.set(0);
+    }
 
     return(
         <article className='event-card'>
@@ -9,9 +41,11 @@ function EventCard () {
                 <div className='event-card__image--container'>
                     <img className='event-card__image' src="https://placehold.co/500x200" alt="Event background image" />
                 </div>
-                <div className='event-card__poster--container'>
-                    <img className='event-card__poster' src="https://placehold.co/200x350" alt="Event poster" />
-                </div>
+
+                {/* HERE is our framer motion.div that takes our mouse position to move the poster */}
+                <motion.div onMouseMove={handleMouseMove} onMouseLeave={handleMouseLeave} style={{rotateX, rotateY, transformStyle: 'preserve-3d',}} className='event-card__poster--container'>
+                    <img className='event-card__poster' src={"https://placehold.co/200x350"} alt="Event poster" />
+                </motion.div>
 
                 <div className='event-card__text--container'>
                     <p className='event-card__text'>text for description</p>
