e2020bb04fcc63de2e811156fe50eb70e795a534
jackstasiulis <jackstasiulis@gmail.com>
2023-12-02 13:04:59 -0500

    lay out responsive card component design

--- src/Components/EventCard/EventCard.jsx
+++ src/Components/EventCard/EventCard.jsx
@@ -4,9 +4,23 @@ function EventCard () {
 
 
     return(
-        <body className='event-card'>
-            <h1>EventCard</h1>
-        </body>
+        <article className='event-card'>
+            <div className='event-card__content'>
+                <div className='event-card__image--container'>
+                    <img className='event-card__image' src="https://placehold.co/500x200" alt="Event background image" />
+                </div>
+                <div className='event-card__poster--container'>
+                    <img className='event-card__poster' src="https://placehold.co/200x350" alt="Event poster" />
+                </div>
+
+                <div className='event-card__text--container'>
+                    <p className='event-card__text'>text for description</p>
+                    <p className='event-card__text'>text for description</p>
+                    <p className='event-card__text'>text for description</p>
+                    <p className='event-card__text'>text for description</p>
+                </div>
+            </div>
+        </article>
     )
 }
 
--- src/Components/Events/Events.jsx
+++ src/Components/Events/Events.jsx
@@ -5,10 +5,12 @@ function Events () {
 
 
     return(
-        <body className='events'>
+        <section className='events'>
             <h1>events</h1>
             <EventCard />
-        </body>
+            <EventCard />
+
+        </section>
     )
 }
 
