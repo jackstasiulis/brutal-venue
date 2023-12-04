a4aa667b7e6e01950cb336e50ed011d0918b6ca3
jackstasiulis <jackstasiulis@gmail.com>
2023-12-03 19:55:15 -0500

    add font change capability to the hero title text

--- src/Components/Hero/Hero.jsx
+++ src/Components/Hero/Hero.jsx
@@ -1,7 +1,40 @@
+import { useEffect, useState } from 'react';
 import './Hero.scss'
+
 
 function Hero () {
 
+    // const [fontIndex, setFontIndex] = useState(0);
+    const fontOptions = ['Redaction', 'Redaction10', 'Redaction20', 'Redaction35', 'Redaction50', 'Redaction70', 'Redaction100'];
+
+    const [fontIndex, setFontIndex] = useState(fontOptions.length - 1);
+
+    useEffect(() => {
+        const interval = setInterval(() => {
+          setFontIndex((prevIndex) => {
+            const newIndex = (prevIndex - 1 + fontOptions.length) % fontOptions.length;
+            if (newIndex === 0) {
+              clearInterval(interval); // this stops the interval when our first font is reached
+            }
+            return newIndex;
+          });
+        }, 80); // Interval at which our font change loads in with
+    
+        return () => clearInterval(interval);
+      }, []);
+
+    const handleMouseMove = (event) => {
+      const titleElement = document.getElementById('heroText');
+      const mouseX = event.clientX;
+      const rect = titleElement.getBoundingClientRect();
+  
+      // this checks if the mouse is over the title!
+      if (mouseX >= rect.left && mouseX <= rect.right) {
+        const percentage = (mouseX - rect.left) / (rect.width || 1);
+        const newIndex = Math.floor(percentage * fontOptions.length);
+        setFontIndex(newIndex);
+      }
+    };
 
     return(
         <body className='hero'>
@@ -10,8 +43,8 @@ function Hero () {
                     <img className='hero__image' src="https://placehold.co/1000x600" alt="DO THIS LATER" />
                 </article>
                 
-                <article className='hero__text--container'>
-                    <h1 className='hero__text'>placeholder</h1>
+                <article id='heroText' className='hero__text--container' onMouseMove={handleMouseMove}>
+                    <h1 className='hero__text' style={{ fontFamily: fontOptions[fontIndex] }}>placeholder</h1>
                 </article>
             </div>
         </body>
