6b50b6de79abef7aa9c0f453b3dd5ab14a07c982
jackstasiulis <jackstasiulis@gmail.com>
2023-12-06 10:51:52 -0500

    further build out footer section

--- src/Components/Footer/Footer.jsx
+++ src/Components/Footer/Footer.jsx
@@ -11,18 +11,18 @@ function Footer () {
 
             <div className='footer__options'>
                 <article className='footer__options--left'>
-                    <p className='footer__option'>option</p>
-                    <p className='footer__option'>option</p>
+                    <p className='footer__option'>contact</p>
+                    <p className='footer__option'>WORK</p>
                 </article>
 
                 <article className='footer__options--right'>
-                    <p className='footer__option'>option</p>
-                    <p className='footer__option'>option</p>
+                    <p className='footer__option'>FIND</p>
+                    <p className='footer__option'>ABOUT</p>
                 </article>
             </div>
 
             <div className='footer__copy--container'>
-                <p className='footer__copy'>copy</p>
+                <p className='footer__copy'>LOREM&copy; 2024</p>
             </div>
 
 
