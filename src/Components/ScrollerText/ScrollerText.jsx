f2cffc3c9683cb7076287c177b2337e81ebba4b2
jackstasiulis <jackstasiulis@gmail.com>
2023-12-13 14:34:00 -0500

    finalize side scroll text component

--- src/Components/ScrollerText/ScrollerText.jsx
+++ src/Components/ScrollerText/ScrollerText.jsx
@@ -6,23 +6,16 @@ function ScrollerText () {
 
     return(
         <div className='scroller-text'>
-                <Marquee>
+                <Marquee speed={40} className='marq'>
                     <div className='scroller-text__bar'>
-                        <div className='scroller-text__text--container'>
+                        {/* <div className='scroller-text__text--container'>
                             <h3 className='scroller-text__text'>SHOWS HAPPENING EVERYDAY, STAY IN THE LOOP.</h3>
-                        </div>
-                        <div className='scroller-text__text--container'>
-                            <h3 className='scroller-text__text'>SHOWS HAPPENING EVERYDAY, STAY IN THE LOOP.</h3>
-                        </div>
-                        <div className='scroller-text__text--container'>
-                            <h3 className='scroller-text__text'>SHOWS HAPPENING EVERYDAY, STAY IN THE LOOP.</h3>
-                        </div>
-                        <div className='scroller-text__text--container'>
-                            <h3 className='scroller-text__text'>SHOWS HAPPENING EVERYDAY, STAY IN THE LOOP.</h3>
-                        </div>
-                        <div className='scroller-text__text--container'>
-                            <h3 className='scroller-text__text'>PLEASE COME TO THE SHOW.</h3>
-                        </div>
+                        </div> */}
+                        <h3 className='scroller-text__text'>ショーズ・ハペニング・エブリッデー・ステー・イン・ザー・ループ</h3>
+                        <h3 className='scroller-text__text'>ショーズ・ハペニング・エブリッデー・ステー・イン・ザー・ループ</h3>
+                        <h3 className='scroller-text__text'>ショーズ・ハペニング・エブリッデー・ステー・イン・ザー・ループ</h3>
+                        <h3 className='scroller-text__text'>ショーズ・ハペニング・エブリッデー・ステー・イン・ザー・ループ</h3>
+                        <h3 className='scroller-text__text'>ショーズ・ハペニング・エブリッデー・ステー・イン・ザー・ループ</h3>
                     </div>
                 </Marquee>
 
