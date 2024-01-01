f387b442e92ccab3514d75192ac30b8e5e3f2c44
jackstasiulis <jackstasiulis@gmail.com>
2024-01-01 16:45:56 -0500

    fix navbar coloring issues and hamburger screensize closure

--- src/Components/Navbar/Navbar.jsx
+++ src/Components/Navbar/Navbar.jsx
@@ -1,6 +1,6 @@
 import DarkLightToggle from '../DarkLightToggle/DarkLightToggle';
 import './Navbar.scss'
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
 
 function Navbar ({isDarkMode, toggleTheme}) {
     // state var for HAMBURGER menu
@@ -8,7 +8,38 @@ function Navbar ({isDarkMode, toggleTheme}) {
 
     const toggleMenu = () => {
       setMenuOpen(!menuOpen);
+    
+    //   if (window.innerWidth <= 768) {
+    //     setMenuOpen(!menuOpen);
+    //   } else {
+    //     setMenuOpen(false)
+    //     console.log('menuCLOSED')
+    //   }
     };
+
+
+    useEffect(() => {
+        const handleResize = () => {
+          if (window.innerWidth <= 768) {
+            // Only set menu state to true if the screen width is less than or equal to 768 pixels
+            setMenuOpen();
+          } else {
+            // Set menu state to false if the screen width exceeds 768 pixels
+            setMenuOpen(false);
+          }
+        };
+    
+        // Add event listener for window resize
+        window.addEventListener('resize', handleResize);
+    
+        // Initial call to handleResize to set the initial state based on the window width
+        handleResize();
+    
+        // Cleanup event listener on component unmount
+        return () => {
+          window.removeEventListener('resize', handleResize);
+        };
+      }, []); // Empty dependency array ensures that the effect
 
     return(
         <nav className={`navbar ${menuOpen ? 'open' : ''}`}>
